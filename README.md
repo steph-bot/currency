4d160197989c2b0429e303b6351ff1a2fcc919ae
Stephanie Mathew <stephanie.mathew@liongard.com>
2020-08-06 20:23:59 -0500

    Front End Skeleton Templates and Client Side Routing

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Single Page Application
 
 This is a project demo that uses Vanilla JS to build a Single Page Application.
+
+Reference: https://www.sitepoint.com/single-page-app-without-framework/
+
+To get up and running: 
+- `npm install` and `npm run start` in terminal
+- navigate to http://localhost:3000/ in browser
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.static('public'));
 // Allow front-end access to node_modules folder
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
+// Redirect all traffic to index.html
+app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
     console.log('listening on %d', port);
